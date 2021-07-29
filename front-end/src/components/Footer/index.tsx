bd12823b16b69ba14f8ac038a9995a5c1f24f21f
Matozinho <matozinhof.academico@gmail.com>
2021-07-29 12:09:30 -0300

    (feat): create footer component
    Signed-off-by: Matozinho <matozinhof.academico@gmail.com>

---
+++ front-end/src/components/Footer/index.tsx
@@ -0,0 +1,51 @@
+/* eslint-disable @next/next/no-img-element */
+import Image from 'next/image'
+
+import styles from './styles.module.scss';
+
+export const Footer = (): JSX.Element => {
+
+  const handleCopyEmail = (): void => {
+    navigator.clipboard.writeText("fndechecker@gmail.com");
+  }
+
+  return (
+    <footer className={styles.container}>
+      <div className={styles.content}>
+        <Image
+          src='/darkLogo.svg'
+          width={138}
+          height={64}
+          alt="Logo do FNDEChecker"
+        />
+
+        <div className={styles.contact}>
+          <div id={styles.github} className={styles.contactIcons}>
+            <a
+              href="https://github.com/Matozinho/FNDEChecker/tree/main/front-end"
+              target="_blank"
+              rel="noopener noreferrer"
+            >
+              <Image
+                src='/githubIcon.svg'
+                width={88}
+                height={69}
+                alt="Entrar em contato via email"
+              />
+            </a>
+          </div>
+          <div id={styles.email} className={styles.contactIcons}>
+            <button onClick={handleCopyEmail}>
+              <Image
+                src='/emailIcon.svg'
+                width={88}
+                height={69}
+                alt="Entrar em contato via email"
+              />
+            </button>
+          </div>
+        </div>
+      </div>
+    </footer>
+  )
+}
--- front-end/src/pages/index.tsx
+++ front-end/src/pages/index.tsx
@@ -1,9 +1,12 @@
 import styles from '@styles/Home.module.scss';
 
+import { Footer } from '@components/Footer';
+
 export default function Home(): JSX.Element {
   return (
     <div className={styles.container}>
       <h1>FNDEChecker begining</h1>
+      <Footer />
     </div>
   )
 }
